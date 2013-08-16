d31c594d398cd9898557925fd5e7249947907c43
William Drai <william.drai@graniteds.org>
2013-08-16 17:43:08 +0200

    Step 1b: cleanup of archetype

--- javafx/src/main/java/com/wineshop/client/Home.java
+++ javafx/src/main/java/com/wineshop/client/Home.java
@@ -21,31 +21,13 @@
 package com.wineshop.client;
 
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.util.Callback;
 
-import javax.inject.Inject;
-
-import org.granite.client.tide.data.EntityManager.UpdateKind;
-import org.granite.client.tide.server.EmptyTideResponder;
-import org.granite.client.tide.server.SimpleTideResponder;
-import org.granite.client.tide.server.TideFaultEvent;
-import org.granite.client.tide.server.TideResultEvent;
 import org.granite.client.tide.spring.TideApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-
-import com.wineshop.client.entities.Welcome;
-import com.wineshop.client.services.WelcomeService;
 
 
 /**
@@ -55,84 +37,12 @@ import com.wineshop.client.services.WelcomeService;
 @Component
 public class Home implements Initializable, ApplicationListener<TideApplicationEvent> {
 
-	@FXML 
-	private TextField fieldHello;
-	
-	@FXML 
-	private ListView<Welcome> listWelcomes;
-	
-	@FXML 
-	private Label labelMessage;
-	
-	@Inject
-	private WelcomeService welcomeService;
-	
-	
-	@SuppressWarnings("unused")
-	@FXML
-	private void hello(ActionEvent event) {
-	    /**
-	     * Issue a remote call to the welcome service and display result or error
-	     */
-		welcomeService.hello(fieldHello.getText(), 
-			new SimpleTideResponder<Welcome>() {
-				@Override
-				public void result(TideResultEvent<Welcome> tre) {
-					fieldHello.setText("");
-					labelMessage.setVisible(false);
-					labelMessage.setManaged(false);
-				}
-				
-				@Override
-				public void fault(TideFaultEvent tfe) {
-					labelMessage.setText("Error: " + tfe.getFault().getFaultDescription());
-					labelMessage.setVisible(true);
-					labelMessage.setManaged(true);
-				}
-			}
-		);
-	}
-	
-	
 	@Override
 	public void onApplicationEvent(TideApplicationEvent event) {
-	    /**
-	     * Handle PERSIST messages received from the real-time topic
-	     */
-		if (event.getType().equals(UpdateKind.PERSIST.eventName(Welcome.class)))
-			listWelcomes.getItems().add((Welcome)event.getArgs()[0]);
 	}
 	
 
 	@Override
 	public void initialize(URL url, ResourceBundle bundle) {
-	    /**
-	     * Attach a bound text cell to the list view so remote updates can be done
-	     * through data binding
-	     */
-		listWelcomes.setCellFactory(new Callback<ListView<Welcome>, ListCell<Welcome>>() {
-			@Override
-			public ListCell<Welcome> call(ListView<Welcome> listView) {
-				return new ListCell<Welcome>() {
-					protected void updateItem(Welcome welcome, boolean empty) {
-						Welcome oldWelcome = getItem();
-						if (oldWelcome != null && welcome != oldWelcome)
-							textProperty().unbindBidirectional(oldWelcome.messageProperty());
-						
-						super.updateItem(welcome, empty);
-						
-						if (welcome != null && welcome != oldWelcome)
-							textProperty().bindBidirectional(welcome.messageProperty());
-					}
-				};
-			}
-		});
-		
-		/**
-		 * Trigger a remote call to find current list of welcome objects
-		 * Use the mergeWith property to automatically merge the results with the 
-		 * collection bound to the ListView
-		 */
-		welcomeService.findAll(EmptyTideResponder.<List<Welcome>>mergeWith(listWelcomes.getItems()));
 	}
 }
