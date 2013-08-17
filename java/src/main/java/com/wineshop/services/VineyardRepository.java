b87875f41552edc8614dec1ed012bb7ae019cd02
William Drai <william.drai@graniteds.org>
2013-08-17 19:20:33 +0200

    Step 7: Real-time data synchronization

--- java/src/main/java/com/wineshop/services/VineyardRepository.java
+++ java/src/main/java/com/wineshop/services/VineyardRepository.java
@@ -2,12 +2,13 @@ package com.wineshop.services;
 
 import org.granite.messaging.service.annotations.RemoteDestination;
 import org.granite.tide.data.DataEnabled;
+import org.granite.tide.data.DataEnabled.PublishMode;
 import org.granite.tide.spring.data.FilterableJpaRepository;
 
 import com.wineshop.entities.Vineyard;
 
 @RemoteDestination
-@DataEnabled
+@DataEnabled(topic="wineshopTopic", publish=PublishMode.ON_SUCCESS)
 public interface VineyardRepository
     extends FilterableJpaRepository<Vineyard, Long> {
 }
