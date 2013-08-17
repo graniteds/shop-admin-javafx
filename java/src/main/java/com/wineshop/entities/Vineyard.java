25a72f3f81dad1fa0c2d655a193d403547a8e562
William Drai <william.drai@graniteds.org>
2013-08-17 15:56:07 +0200

    Step 5: Validation

--- java/src/main/java/com/wineshop/entities/Vineyard.java
+++ java/src/main/java/com/wineshop/entities/Vineyard.java
@@ -7,6 +7,9 @@ import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Vineyard extends AbstractEntity {
@@ -14,15 +17,19 @@ public class Vineyard extends AbstractEntity {
     private static final long serialVersionUID = 1L;
  
     @Basic
+    @NotNull
+    @Size(min=5, max=100, message="The name must contain between {min} and {max} characters")
     private String name;
  
     @Embedded
+    @Valid
     private Address address = new Address();
  
     @OneToMany(cascade=CascadeType.ALL, mappedBy="vineyard",
         orphanRemoval=true)
+    @Valid
     private List<Wine> wines;
- 
+    
     public String getName() {
         return name;
     }
--- java/src/main/java/com/wineshop/entities/Wine.java
+++ java/src/main/java/com/wineshop/entities/Wine.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Wine extends AbstractEntity {
@@ -21,12 +24,17 @@ public class Wine extends AbstractEntity {
     private Vineyard vineyard;
  
     @Basic
+    @Size(min=5, max=100,
+    	message="The name must contain between {min} and {max} characters")
     private String name;
  
     @Basic
+    @Min(value=1900,
+    	message="The year must be greater than {value}")
     private Integer year;
  
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Type type;
  
     public Vineyard getVineyard() {
